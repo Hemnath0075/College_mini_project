7efa2519c881d7830e67cbd88010d1cb93537762
Hemnath0075 <hemnath5700@gmail.com>
2021-12-04 13:53:58 +0530

    saturday commit morning

--- insert.php
+++ insert.php
@@ -5,7 +5,7 @@ include ('connection.php');
 extract($_POST);
 
 if(isset($_POST['regnosend']) && isset($_POST['usernamesend']) && isset($_POST['passwordsend'])){
-    $sql="insert into `userstudent` (regno,password,username) values ('$regnosend','$usernamesend','$passwordsend')";
+    $sql="insert into `userstudent` (regno,password,username) values ('$regnosend','$passwordsend','$usernamesend')";
     $result=mysqli_query($con,$sql);
 }
 
