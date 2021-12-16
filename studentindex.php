0c0963db141f8be86f1b5be61e259a21b7e68e19
Hemnath0075 <hemnath5700@gmail.com>
2021-12-16 21:33:12 +0530

    date is 16 th dec

--- studentindex.php
+++ studentindex.php
@@ -45,6 +45,70 @@ session_start();
 	<br>
 
 	hello welcome <?php echo $user_data['username']; ?>
+	
+	<h1>TO DO LIST FOR STUDENTS</h1>
+	<form style="border: 2px solid black; width:50vw; margin-left:20rem; padding:10px;" method="post">
+	<div class="mb-3">
+		<label for="task">enter your task:</label>
+		<input type="text" name="task">
+	</div>
+	<div class="mb-3">
+		<label for="taskdescription">enter your task description:</label>
+		<input type="text" name="taskdescription">
+	</div>
+	<div class="mb-3">
+		<label for="date">enter the date to complete:</label>
+		<input type="date" name="date">
+	</div>
+		<button class="btn btn-primary" name="submit" type="submit">Create Task</button>
+
+		<?php
+		if(isset($_POST['submit'])){
+		$task=$_POST['task'];
+		$taskdescription=$_POST['taskdescription'];
+		$date=date('Y-m-d' , strtotime($_POST['date']));
+		$regno=$_SESSION['regno'];
+		$sql="insert into `task` (task,taskdescription,date,regno) values('$task','$taskdescription','$date',$regno)";
+		$query=mysqli_query($con,$sql);
+		}
+// 		echo "Today is " . date("Y/m/d") . "<br>";
+// 		$start = strtotime('2021-01-20');
+// $end = strtotime('2021-01-01');
+
+// $days_between = ceil(abs($end - $start) / 86400);
+
+echo $days_between;
+		?>
+
+
+	</form>
+	<div class="container">
+	<?php
+	$table="table";
+	$regno=$_SESSION['regno'];
+	$start =strtotime(date("Y-m-d"));
+	echo $start;
+	echo "<table class=$table>";
+	echo "<tr>
+	<th>Task Name</th>
+	<th>Task Description</th>
+	<th>Remaining Days</th>
+	<th>Status</th>
+	</tr>";
+	$sql="select * from `task` where regno=$regno";
+	$query=mysqli_query($con,$sql);
+	
+	while($row=mysqli_fetch_array($query)){
+		$end=strtotime($row['date']);
+		$days_between = ceil(abs($end - $start) / 86400);
+		echo "<tr><td>".$row['task']."</td><td>".$row['taskdescription']."</td><td>".$days_between."</td></tr>";
+	}
+
+	
+
+
+	?>
+</div>
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
